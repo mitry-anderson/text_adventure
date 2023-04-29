11862038cbb6d052865a958096edf400bf9a6331
mitryand <mitryand@umich.edu>
2023-04-29 09:53:07 -0500

    doc parsing

--- book_parsing/bible_parse.py
+++ book_parsing/bible_parse.py
@@ -1,6 +1,7 @@
 import re
 from docx import Document
-from docx.shared import Inches
+from docx.shared import Inches, Pt, RGBColor
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 folder = "kjv"
 filename = "mark"
@@ -11,27 +12,43 @@ chapter_regex = r"\d*(?=:)"
 chapter_and_verse_regex = r"[\d*:\d*]"
 
 output_doc = Document()
-output_doc.add_heading(filename, 0)
 
+
+title = output_doc.add_paragraph()
+title_run = title.add_run("\n" + filename.upper())
+title_font = title_run.font
+title_font.name = "Baskerville Old Face"
+title_font.size = Pt(90)
+title_font.bold = True
+title_font.underline = False
+title_font.color.rgb = RGBColor(0x00, 0x00, 0x00)
+title.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
 def write_paragraph(output_line):
     f_out.write(output_line + "\n")
-    output_doc.add_paragraph(output_line)
+    para = output_doc.add_paragraph().add_run(output_line)
+    para.font.name = "Baskerville Old Face"
+    para.font.size = Pt(14)
     print(output_line)
-
 
 def write_chapter(num):
-    output_line = "\nCHAPTER " + str(chapter_num) + "\n"
+    output_doc.add_page_break()
+    output_line = "\nCHAPTER " + str(num)
     f_out.write(output_line + "\n")
-    output_doc.add_heading(output_line, level=1)
+    head = output_doc.add_paragraph()
+    head.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    head_run = head.add_run(output_line)
+    head_font = head_run.font
+    head_font.name = "Baskerville Old Face"
+    head_font.size = Pt(20)
+    head_font.bold = True
+    head_font.underline = False
+    head_font.color.rgb = RGBColor(0x00, 0x00, 0x00)
     print(output_line)
-
 
 with open(folder + "/" + filename + ".txt", "r") as f_in:
     with open(folder + "/" + "edited_" + filename + ".txt", "w") as f_out:
-        output_line = "CHAPTER " + str(chapter_num) + "\n"
-        f_out.write(output_line + "\n")
-        output_doc.add_heading(output_line, level=1)
+        write_chapter(chapter_num)
         output_line = ""
         while(run):
             line = f_in.readline()
